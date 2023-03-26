853acbff3d2f81a0c0e4268d26c05866b5161a08
Nero-o <omarmahmoud3611@gmail.com>
2023-03-26 17:46:12 -0300

    feat: exercices 42 - 63

---
+++ Javascript Basics/45-shopping-list.js
@@ -0,0 +1,9 @@
+var myList = [];
+
+myList = [
+  ["cholate", 1],
+  ["leite", 2],
+  ["agua", 3],
+  ["arroz", 4],
+  ["feijão", 5],
+];
---
+++ Javascript Basics/52-understanding-undefined-value-returned-from-a-function.js
@@ -0,0 +1,11 @@
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
